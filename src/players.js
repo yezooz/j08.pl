599f90193265b7fc46e92b987aa24682cfa93e82
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2016-12-08 20:55:48 +0100

    Player

--- src/players.js
+++ src/players.js
@@ -1,118 +1,151 @@
-import { GOALKEEPER, DEFENDER, MIDFIELDER, ATTACKER } from './components/player_position';
+import { GOALKEEPER, DEFENDER, MIDFIELDER, ATTACKER } from './player_position';
+import { LEFT, RIGHT } from './side';
 
 export default const players = [
   {
     number: 15,
     full_name: "Mateusz Bąk",
     name: "Bąk",
-    position: MIDFIELDER
+    position: MIDFIELDER,
+    foot: RIGHT
   },
   {
     number: 20,
     full_name: "Nikodem Bronicki",
     name: "Brodnicki",
-    position: MIDFIELDER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: RIGHT
   },
   {
     number: 21,
     full_name: "Oliwier Gardynik",
     name: "Gardynik",
-    position: DEFENDER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 11,
     full_name: "Szymon Gościski",
     name: "Gościcki",
-    position: ATTACKER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 17,
     full_name: "Robert Janicki",
     name: "Janicki",
-    position: ATTACKER
+    position: ATTACKER,
+    foot: LEFT,
+    side: LEFT
   },
   {
     number: 5,
     full_name: "Maksymilian Kłodarski",
     name: "Kłodarski",
-    position: DEFENDER
+    position: DEFENDER,
+    foot: RIGHT,
+    side: RIGHT
   },
   {
     number: 19,
     full_name: "Anton Kowaluk",
     name: "Kowaluk",
-    position: DEFENDER
+    position: DEFENDER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 6,
     full_name: "Oliwier Łaskarzewski",
     name: "Łaskarzewski",
-    position: MIDFIELDER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 3,
     full_name: "Jordan Maciejewski",
     name: "Maciejewski",
-    position: GOALKEEPER
+    position: GOALKEEPER,
+    foot: RIGHT
   },
   {
     number: 10,
     full_name: "Jakub Malik",
     name: "Malik",
-    position: MIDFIELDER
+    position: GOALKEEPER,
+    foot: RIGHT
   },
   {
     number: 18,
     full_name: "Kajetan Michałowski",
     name: "Michałowski",
-    position: DEFENDER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: RIGHT
   },
   {
     number: 16,
     full_name: "Nikodem Mikuliszyn",
     name: "Mikuliszyn",
-    position: MIDFIELDER
+    position: MIDFIELDER,
+    foot: RIGHT
   },
   {
     number: 8,
     full_name: "Aleksander Olejnik",
     name: "Olejnik",
-    position: MIDFIELDER
+    position: ATTACKER,
+    foot: LEFT,
+    side: LEFT
   },
   {
     number: 14,
     full_name: "Jakub Pishchanskyy",
     name: "Pishchanskyy",
-    position: DEFENDER
+    position: DEFENDER,
+    foot: RIGHT,
+    side: RIGHT
   },
   {
     number: 4,
     full_name: "Daniel Rutkowski",
     name: "Rutkowski",
-    position: MIDFIELDER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 7,
     full_name: "Adam Szwajkowski",
     name: "Szwajkowski",
-    position: ATTACKER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 9,
     full_name: "Filip Szymański",
     name: "Szymański",
-    position: ATTACKER
+    position: MIDFIELDER,
+    foot: RIGHT
   },
   {
     number: 2,
     full_name: "Tomasz Tudies",
     name: "Tudies",
-    position: ATTACKER
+    position: ATTACKER,
+    foot: RIGHT,
+    side: LEFT
   },
   {
     number: 13,
     full_name: "Jakub Zieliński",
     name: "Zieliński",
-    position: GOALKEEPER
+    position: GOALKEEPER,
+    foot: RIGHT
   }
 ];
